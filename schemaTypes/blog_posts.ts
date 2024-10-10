dbc604b42c9214f3a3c4f786b4ddb1ba2ad52ccf
Ritwik12082001 <ritwik.c@techouts.com>
2024-10-10 10:17:21 +0530

    Add files via upload

---
+++ schemaTypes/blog_posts.ts
@@ -0,0 +1,29 @@
+export const blogPosts = {
+    name: 'blog_contents',
+    title: 'Blog Contents',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title'
+        },
+        {
+            title: 'Content',
+            name: 'content',
+            type: 'array',
+            of: [{ type: 'block' }]
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image'
+        },
+        {
+          name: 'employee',
+          title: 'Employee',
+          type: 'reference',
+          to: [{type:'employee'}]
+        }
+    ]
+}
